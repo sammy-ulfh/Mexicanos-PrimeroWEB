c2dc854ee5b125257750e7d79a55f74832565e5d
Pavopalomo <A01067911@tec.mx>
2025-05-01 22:48:16 -0600

    Admin Apoyo

--- BackEnd/controllers/apoyoController.js
+++ BackEnd/controllers/apoyoController.js
@@ -28,14 +28,13 @@ const newApoyo = async (req, res) => {
 const cambiarStatus = async (req, res) => {
     try {
         const { id_apoyo,  status, razon_rechazo  } = req.body;
-        console.log(req.body);
         if(status==3){
             await Apoyo.cambiarStatus(id_apoyo, null, status, razon_rechazo);
-            res.status(201).json({ mensaje: 'Información almacenada', redirigir: 'http://localhost:5173/admin/apoyos' });
+            res.status(201).json({ mensaje: 'Información almacenada', redirigir: '../admin/apoyos' });
         }
         else if(status==2){
             await Apoyo.cambiarStatus(id_apoyo, req.payload.id_usuario, status, razon_rechazo);
-            res.status(201).json({ mensaje: 'información almacenada', redirigir:'/admin/apoyos'});
+            res.status(201).json({ mensaje: 'información almacenada', redirigir:'../admin/apoyos'});
         }
     } catch (error) {
         console.error('Error al guardar la información:', error);
