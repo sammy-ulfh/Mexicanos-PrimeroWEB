1db95ce05001774aef501524027ca2c4ad841bc6
sammy-ulfh <sammy_ulfh@proton.me>
2025-05-02 04:22:39 -0600

    Chats 2 personas - Update

--- BackEnd/models/chat_participantesModel.js
+++ BackEnd/models/chat_participantesModel.js
@@ -31,32 +31,47 @@ const eliminarParticipante = async (id_chat, id_usuario) => {
 const getChats = async (id) => {
   const [result] = await db.execute(
     `SELECT 
-  cp.id_chat,
-  cp.id_escuela,
-  ie.nombre AS nombre_escuela,
-  cp.id_donante,
-  id.nombre AS nombre_donante,
-  cp.id_admin,
-  u.correo AS nombre_admin  -- o puedes usar otro campo si admin tiene su propio detalle
-FROM chat_participantes cp
-LEFT JOIN info_escuela ie ON cp.id_escuela = ie.id_usuario
-LEFT JOIN info_donador id ON cp.id_donante = id.id_usuario
-LEFT JOIN usuarios u ON cp.id_admin = u.id_usuario
-WHERE (
-    cp.id_escuela = ? OR 
-    cp.id_donante = ? OR 
-    cp.id_admin = ?
-)
-AND (
-    (cp.id_escuela IS NOT NULL) +
-    (cp.id_donante IS NOT NULL) +
-    (cp.id_admin IS NOT NULL)
-) = 2;
-`,
-    [id, id, id]
-  );
+      cp.id_chat,
+      cp.id_escuela,
+      u_escuela.nombre AS nombre_escuela,
+      u_escuela.img AS img_escuela,
+      cp.id_donante,
+      u_donante.nombre AS nombre_donante,
+      u_donante.img AS img_donante,
+      cp.id_admin,
+      u_admin.nombre AS nombre_admin,
+      u_admin.img AS img_admin,
+      m.contenido AS ultimo_mensaje,
+      m.fecha_envio AS fecha_ultimo_mensaje
+    FROM chat_participantes cp
+    LEFT JOIN usuarios u_escuela ON cp.id_escuela = u_escuela.id_usuario
+    LEFT JOIN usuarios u_donante ON cp.id_donante = u_donante.id_usuario
+    LEFT JOIN usuarios u_admin   ON cp.id_admin   = u_admin.id_usuario
+    LEFT JOIN (
+        SELECT m1.*
+        FROM mensajes m1
+        INNER JOIN (
+            SELECT id_chat, MAX(fecha_envio) AS max_fecha
+            FROM mensajes
+            GROUP BY id_chat
+        ) m2 ON m1.id_chat = m2.id_chat AND m1.fecha_envio = m2.max_fecha
+    ) m ON cp.id_chat = m.id_chat
+    WHERE (
+        cp.id_escuela = ? OR 
+        cp.id_donante = ? OR 
+        cp.id_admin = ?
+    )
+    AND (
+        (cp.id_escuela IS NOT NULL) +
+        (cp.id_donante IS NOT NULL) +
+        (cp.id_admin IS NOT NULL)
+    ) = ?;`,
+    [id, id, id, 2]
+  );  
+  
   return result;
 };
+
 
 module.exports = {
     agregarParticipante,
--- Front-End/src/client/Pages/Chats.jsx
+++ Front-End/src/client/Pages/Chats.jsx
@@ -1,34 +1,10 @@
 import '@fontsource/montserrat';
 import MainLayout from '../Layouts/MainLayout';
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TarjetaProcesos from '../../GeneralComponents/TarjetaProcesos';
 import TarjetaChats from '../../GeneralComponents/TarjetaChats';
 import getChats from '../Peticiones/chats/getChats';
-
-const Chats = [
-  {
-      chat_id: 1,
-      user_id: 2,
-      rol: 'school',
-      info: {
-        'Escuela': {
-        id: 1,
-        name: 'Miguel Hidalgo y Costilla de Sabrade Robles',
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.recreoviral.com%2Fwp-content%2Fuploads%2F2015%2F01%2Fmitad-2-personas-4.jpg&f=1&nofb=1&ipt=a7b13ff6fcf7dabb797806f3cb1aa6da660c7c25a92b157dd688336252bab819',
-        type: 'school'
-        },
-        'Donador': {
-        id: 2,
-        name: 'Jose Pepe',
-        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcovalto.com%2Fstatic%2F78498ccda70933a5f1e3edc3e40d3cbe%2F34aca%2FHero_Mobile_Cuenta_Personas_V1_1_8046e424ea.webp&f=1&nofb=1&ipt=59a7bca374102c037d0917182ea9914e652a59f006a6afe653ad45790c6088da',
-        type: 'donator'
-        }
-      },
-      last_message: 'El proceso de donación que recibiremos en este caso será de libros. Esto lo usaremos para poder crear una pequeña biblioteca de la cual los niños puedan tomar libros para leerlos y aprender cosas nuevas.'
-  }
-    ]
-
 
 const Procesos = [
   {
@@ -131,10 +107,28 @@ const Procesos = [
 function ClientChats() {
 
     const [isProcess, setIsProcess] = useState(true);
+    const [Chats, setChats] = useState([]);
+
     const navigate = useNavigate();
 
-    const user_rol = 'school';
-    getChats();
+    const id = Number(localStorage.getItem('id'));
+    const type = Number(localStorage.getItem('type'));
+    console.log(Chats);
+
+    useEffect(() => {
+      async function fetchChats() {
+        try {
+          const data = await getChats();
+          setChats(data);
+        } catch (error) {
+          console.error("Error loading chats", error);
+        } finally {
+          setIsProcess(false);
+        }
+      }
+  
+      fetchChats();
+    }, []);
 
     return (
       <>
@@ -161,21 +155,17 @@ function ClientChats() {
                       last_message={tarjeta.last_message}
                       navigate={navigate}
                       data={tarjeta.info}
-                      user_rol={user_rol}
+                      user_rol={type}
                     />
                   ))
                 ) : Chats.length > 0 && !isProcess ? (
                   Chats.map((tarjeta) => {
-                    const esEscuela = tarjeta.user_id !== tarjeta.info.Escuela.id;
-                    const info = esEscuela ? tarjeta.info['Escuela'] : tarjeta.info['Donador'];
                     return (
                       <TarjetaChats
-                        user_id={tarjeta.user_id}
-                        chat_id={tarjeta.chat_id}
-                        info={info}
-                        last_message={tarjeta.last_message}
+                        user_id={id}
+                        info={tarjeta}
+                        rol={type}
                         navigate={navigate}
-                        rol={tarjeta.rol}
                       />
                     );
                   })
